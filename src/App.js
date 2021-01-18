e1c07efc84496d5395a9a9e534ba1289886176b2
soham874 <soham874@gmail.com>
2021-01-18 15:08:37 +0530

    Added : Initial setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import 'react-bootstrap-range-slider/dist/react-bootstrap-range-slider.css';
+import RangeSlider from 'react-bootstrap-range-slider';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [ value, setValue ] = useState(0); 
+    return (
+        <div className="App">
+            <header className="App-header">
+                <div class="speed">
+                    
+                </div>
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>Edit <code> src / App.js </code> and save to reload. </p>
+                <a className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer" >
+                    Learn React
+                    </a>
+                    <div class="slider">
+                        <RangeSlider value={value} onChange={changeEvent => setValue(changeEvent.target.value)}/>
+                    </div>
+            </header >
+        </div>      
+    );
 }
 
-export default App;
+export default App
