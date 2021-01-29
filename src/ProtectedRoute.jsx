e361e0f264790e17d55f0016c62fa62ee786028a
soham874 <soham874@gmail.com>
2021-01-29 19:29:07 +0530

    Refactored : Fixed bugs

--- src/ProtectedRoute.jsx
+++ src/ProtectedRoute.jsx
@@ -1,18 +1,16 @@
 import React from 'react'
 import { Redirect } from 'react-router-dom'
 
-class ProtectedRoute extends React.Component {
+export default class ProtectedRoute extends React.Component {
 
     render() {
-        const Component = this.props.component;
+        const Component = this.props.component
         const isAuthenticated = localStorage.getItem('userId');
        
         return isAuthenticated ? (
             <Component />
         ) : (
             <Redirect to={{ pathname: '/login' }} />
-        );
+        )
     }
 }
-
-export default ProtectedRoute;
